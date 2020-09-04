67791f777e8331155dd75b944c5e8874120e6e54
jimdis <jim.disenstam@gmail.com>
2020-09-04 14:08:42 +0200

    Implement hardcoded crude api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,15 +8,39 @@ const router = new Router();
 
 // This is just an example route
 router.get("/sample", (context) => {
-    context.response.body = { message: "Hello world" };
-    context.response.status = 200;
+  context.response.body = { message: "Hello world" };
+  context.response.status = 200;
 });
 
 // Add additional routes for implementation here...
 
-app.use(bodyparser({
+//TODO: Replace with models etc
+let elevators: { number: number; floor: number }[] = [];
+for (let i = 1; i <= 20; i++) {
+  elevators.push({
+    number: i,
+    floor: Math.floor(Math.random() * 20) + 1,
+  });
+}
+router.get("/building", (context) => {
+  context.response.body = { floors: 20, elevators };
+  context.response.status = 200;
+});
+
+router.get("/floor/:number", (context) => {
+  const floorNumber = context.params.number;
+  const elevator = elevators[Math.floor(Math.random() * elevators.length)];
+  const secondsPerFloor = 2;
+  const eta = Math.abs(floorNumber - elevator.floor) * secondsPerFloor;
+  context.response.body = { elevator, eta };
+  context.response.status = 200;
+});
+
+app.use(
+  bodyparser({
     enableTypes: ["json"],
-}));
+  })
+);
 app.use(cors());
 
 app.use(router.routes());
